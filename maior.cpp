a95f1216fc5d1416b8c5e575bf43de97ef64ef16
thiagolj <jesus.alves@aluno.ifsp.edu.br>
2025-06-18 20:16:24 -0300

    Add files via upload

---
+++ maior.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	
+	cout <<" insira o valor de a ";
+	cin >>a;
+	cout <<" insira o valor de b ";
+	cin >>b;
+	
+		if (a>b) {
+	cout << " a maior que b ";
+	} else if (b>a);
+	{
+	cout << " b maior que a ";
+	}
+		
+	return 0;
+}
